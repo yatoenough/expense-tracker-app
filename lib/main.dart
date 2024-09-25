f03a8bc1d68cd789d606f79f275e23486d777c72
Nikita Shyshkin <yatoenough@gmail.com>
2024-09-25 20:54:14 +0200

    added theme settings

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,17 @@ class ExpenseTrackerApp extends StatelessWidget {
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer),
+          backgroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.all(10),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
       ),
       home: const ExpensesScreen(),
     );
