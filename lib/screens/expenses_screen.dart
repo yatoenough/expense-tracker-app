35d77a445d2c1073246bc666c7793cd6cf5b0880
Nikita Shyshkin <yatoenough@gmail.com>
2024-09-25 14:32:30 +0200

    added mock data and implemented listview of expenses

--- lib/screens/expenses_screen.dart
+++ lib/screens/expenses_screen.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/expenses/expenses_list.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesScreen extends StatefulWidget {
@@ -8,11 +10,38 @@ class ExpensesScreen extends StatefulWidget {
 }
 
 class _ExpensesScreenState extends State<ExpensesScreen> {
+  final List<Expense> _expenses = [
+    Expense(
+      title: "Meal",
+      amount: 9.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 18.49,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+    Expense(
+      title: "Plane ticket",
+      amount: 129.99,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        children: [Text("Chart"), Text("Expenses list")],
+        children: [
+          const Text("Chart"),
+          Expanded(
+              child: ExpensesList(
+            expenses: _expenses,
+          )),
+        ],
       ),
     );
   }
