65ed8542e620ba4030210a23a3e9d91ef5b565ba
Nikita Shyshkin <yatoenough@gmail.com>
2024-09-25 14:18:39 +0200

    created category enum and added category attr to expense

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,18 @@
 import 'package:uuid/uuid.dart';
 
+enum Category { food, travel, work, leisure }
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = const Uuid().v4();
 }
