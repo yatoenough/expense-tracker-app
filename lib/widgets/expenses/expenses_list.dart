b94e1f20d945873b69da08d2f874f3f6cae73bb3
Nikita Shyshkin <yatoenough@gmail.com>
2024-09-25 20:05:53 +0200

    implemented remove expense functionality

--- lib/widgets/expenses/expenses_list.dart
+++ lib/widgets/expenses/expenses_list.dart
@@ -3,16 +3,22 @@ import 'package:expense_tracker/widgets/expenses/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onExpenseRemove});
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onExpenseRemove;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (context, index) => ExpenseItem(
-        expense: expenses[index],
+      itemBuilder: (context, index) => Dismissible(
+        key: ValueKey(expenses[index]),
+        onDismissed: (direction) => onExpenseRemove(expenses[index]),
+        child: ExpenseItem(
+          expense: expenses[index],
+        ),
       ),
     );
   }
