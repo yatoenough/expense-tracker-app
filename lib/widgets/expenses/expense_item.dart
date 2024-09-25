8dbf9c9b58a65bb5d592ca070f22f8f72de2581e
Nikita Shyshkin <yatoenough@gmail.com>
2024-09-25 14:36:31 +0200

    created expense item widget

---
+++ lib/widgets/expenses/expense_item.dart
@@ -0,0 +1,15 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem({super.key, required this.expense});
+
+  final Expense expense;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Text(expense.title),
+    );
+  }
+}
