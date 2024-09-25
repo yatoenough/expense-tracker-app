505b4bed5d6d7599546331835e26dfa6fdb677ea
Nikita Shyshkin <yatoenough@gmail.com>
2024-09-25 13:56:59 +0200

    removed boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+    return const MaterialApp(
+      title: 'Flutter Expense Tracker',
     );
   }
 }
